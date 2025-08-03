ec969f6d1ca707d21985c4fa697c6c3564ab7cf6
JosueSur <surjosue@gmail.com>
2025-08-03 19:51:59 -0300

    ya casi

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -101,7 +101,7 @@ const Login = () => {
                 type={showPassword ? "text" : "password"}
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="ya me canse"
+                placeholder="ya casi"
                 value={formData.password}
                 onChange={handleChange}
               />
